cbdbaa7003a183300a0ee9959f4e41d02dccc1be
Blueo0 <ysyangshuang@sina.cn>
2020-09-04 19:18:35 +0800

    Update 源.cpp

--- 源.cpp
+++ 源.cpp
@@ -8,9 +8,9 @@ int main()
 	while (getline(cin, s))
 	{
 		int n = 0, flag = 1;
-		for (int i = s.length() - 1; i >= 0; i--)
+		for (int i = s.length() - 1; i >= 0; i--)//倒着计算
 		{
-			if (flag&&s[i] == ' ')
+			if (flag&&s[i] == ' ')//如果末尾有空格，先清除末尾空格
 			{
 				continue;
 			}
